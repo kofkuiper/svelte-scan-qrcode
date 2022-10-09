1d25219b15c85239c0c7ae7f80f354aca27a1fa5
kofkuiper <kofkuiper@gmail.com>
2022-10-09 14:03:41 +0700

    Update demo with set enableQRCodeReaderButton to true.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,31 @@
 	}
 
 	function _onResulted() {
-		if ($result.includes("love")) {
-			goto("/result");
-		}
+		goto("/result");
 	}
 </script>
 
+<h1>svelte-scan-qrcode Demo</h1>
+<a
+	class="github-link"
+	href="https://github.com/kofkuiper/svelte-scan-qrcode"
+	target="_blank"
+	rel="external"><u>View documentation on Github</u></a
+>
 <ScanQrCode
 	bind:scanResult={$result}
+	enableQRCodeReaderButton={true}
 	options={{
 		onPermissionError: () => _onPermissionError(),
 		onResulted: () => _onResulted(),
 	}}
 />
+
+<style>
+	.github-link {
+		text-align: center;
+		margin-top: 12px;
+		margin-bottom: 24px;
+		color: #2d99ff;
+	}
+</style>
