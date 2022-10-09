20b6933909a32dbdf27fcbb7daac4dbc0a78e0b6
kofkuiper <kofkuiper@gmail.com>
2022-10-09 14:03:07 +0700

    Add QR Code image reader feature.

--- src/lib/components/ScanQRCode.svelte
+++ src/lib/components/ScanQRCode.svelte
@@ -6,7 +6,9 @@
         onPermissionError?: Function;
         onResulted?: Function;
     };
+
     export let scanResult: string = "";
+    export let enableQRCodeReaderButton: boolean;
     export let options: optionsType;
 
     let video: any;
@@ -66,14 +68,64 @@
         canvas.height = video.videoHeight;
         startScan();
     }
+
+    function qrcodeReader(e: any) {
+        try {
+            let file = e.target.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (e: any) => {
+                let img = new Image();
+                img.src = e.target.result;
+                img.onload = () => {
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+                    const context = canvas.getContext("2d");
+                    context.imageSmoothingEnabled = false;
+                    context.drawImage(img, 0, 0);
+                    const imageData = context.getImageData(
+                        0,
+                        0,
+                        img.width,
+                        img.height
+                    );
+                    const qrCode = jsQR(imageData.data, img.width, img.height);
+                    if (qrCode) {
+                        scanResult = qrCode.data;
+                        if (options?.onResulted != undefined) {
+                            options.onResulted(qrCode.data);
+                        } else {
+                            alert(qrCode.data);
+                        }
+                    }
+                };
+            };
+        } catch (error) {
+            console.error(error);
+        }
+    }
 </script>
 
 <canvas bind:this={canvas} />
 <!-- svelte-ignore a11y-media-has-caption -->
 <video on:canplay={onCanPlay} bind:this={video} />
 
+{#if enableQRCodeReaderButton}
+    <input
+        class="image-qrcode-input"
+        type="file"
+        id="upload-qrcode-image"
+        accept=".jpg, .jpeg, .png"
+        on:change={(e) => qrcodeReader(e)}
+    />
+{/if}
+
 <style>
     canvas {
         display: none;
+    }
+
+    .image-qrcode-input {
+        margin-top: 20px;
     }
 </style>
